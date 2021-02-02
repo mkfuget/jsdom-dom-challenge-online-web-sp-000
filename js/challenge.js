2732d51de9a0f98df9fafdcf7695dbad09f13688
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 05:15:24 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,8 @@
+function incrementCounter()
+{
+  document.getElementById('counter').innerHTML += 1;
+}
+
+document.getElementById("plus").addEventListener("click", function() {
+  document.getElementById("demo").innerHTML = "Hello World";
+});
