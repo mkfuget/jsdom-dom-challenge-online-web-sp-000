c8e26f47af16ec5fae5a1cd4af233743f6cdb8bd
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 05:25:27 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,3 +1,5 @@
+const toggleCounter = "on"
+
 function incrementCounter()
 {
   const currentCount = document.getElementById('counter').innerHTML
@@ -18,4 +20,24 @@ document.getElementById("plus").addEventListener("click", function() {
 document.getElementById("minus").addEventListener("click", function() {
   decrementCounter()
 });
-setInterval(incrementCounter, 1000);
+document.getElementById("pause").addEventListener("click", function() {
+  if(toggle === "on")
+  {
+    toggle = "off";
+    document.getElementById("pause").innerHTML = "resume"
+  }
+  else
+  {
+    toggle = "on";
+    document.getElementById("pause").innerHTML = "pause"
+  }
+});
+
+setInterval(function() =>{
+  if(toggle === "on"){
+    incrementCounter()
+
+  }
+
+
+}, 1000);
